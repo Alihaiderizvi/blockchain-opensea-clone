a26b700d289795d20cba77b73557112c351a9161
Alihaiderizvi <alihaiderizvi.you@gmail.com>
2022-05-12 15:41:58 +0500

    Hero Section.

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,14 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import Header from "../components/Header";
+import Hero from "../components/Hero";
 
 const Home = () => {
   return (
-    <Header/>
+    <>
+      <Header/>
+      <Hero/>
+    </>
   )
 }
 
