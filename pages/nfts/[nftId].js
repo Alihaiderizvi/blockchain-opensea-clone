0f03da39017fc29a3db2a0b2e1714c430ac987b3
Alihaiderizvi <alihaiderizvi.you@gmail.com>
2022-05-13 16:23:09 +0500

    Opensea Blockchanin Phase:I

--- pages/nfts/[nftId].js
+++ pages/nfts/[nftId].js
@@ -6,6 +6,7 @@ import { useRouter } from 'next/router'
 import NftImage from '../../components/nft/NftImage'
 import GeneralDetails from '../../components/nft/GeneralDetails'
 import ItemActivity from '../../components/nft/ItemActivity'
+import Purchase from '../../components/nft/Purchase'
 
 const style = {
   wrapper: `flex flex-col items-center container-lg text-[#e5e8eb]`,
@@ -75,12 +76,12 @@ const Nft = () => {
             </div>
             <div className={style.detailsContainer}>
               <GeneralDetails selectedNft={selectedNft} />
-              {/* <Purchase
+              <Purchase
                 isListed={router.query.isListed}
                 selectedNft={selectedNft}
                 listings={listings}
                 marketPlaceModule={marketPlaceModule}
-              /> */}
+              />
             </div>
           </div>
           <ItemActivity />
@@ -91,3 +92,4 @@ const Nft = () => {
 }
 
 export default Nft
+ 
